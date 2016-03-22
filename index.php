01c5d70251df94a971a392c9a2f426ab13dc905e
Despoina <despoina@localhost.localdomain>
2016-03-22 23:29:22 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> Hello Cloudwick </title>
+</head>
+<body>
+<h1> Please enter your names </h1>
+<p>  My website </p>
+<form action ="/list-names.php"method="GET">
+<input type ="text" placeholder = "type your name here" name="name">
+<button type="submit">Submit</button>
+</form>
+
+<div id="container">
+<?php echo "Hello world" ?>
+</div>
+</body>
+</html>
