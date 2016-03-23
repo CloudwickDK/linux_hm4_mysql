ae089672ae977e2fd47d796d0eb33e14d138dbcc
Despoina <despoina@localhost.localdomain>
2016-03-23 16:00:51 +0000

    Typechange from files to links

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<html>
-<head>
-<title> Hello Cloudwick </title>
-</head>
-<body>
-<h1> Please enter your names </h1>
-<p>  My website </p>
-<form action ="/list-names.php" method="POST">
-<input type ="text" placeholder = "type your name here" name="name">
-<button type="submit">Submit</button>
-</form>
-
-<div id="container">
-<?php echo "Hello world" ?>
-</div>
-</body>
-</html>
+/var/www/html/index.php
