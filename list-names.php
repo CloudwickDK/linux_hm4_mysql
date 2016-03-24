b809ce8be86b32176053d97fd8bfd6d2145608f4
Despoina Karna <d.karna@hotmail.com>
2016-03-24 10:48:22 +0000

    uploading files

--- list-names.php
+++ list-names.php
@@ -1,1 +1,37 @@
-/var/www/html/list-names.php
+<?php
+$servername	= "localhost";
+$username	= "cloudwick";
+$password	= "lydiakar";
+$dbname		= "cloudwick";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username =  $_POST['name'];
+
+$sql = "INSERT INTO cloudwick.People (FirstName )
+VALUES ('" . $username . "')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT PersonID, FirstName, LastName FROM People";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "PersonID: " . $row["PersonID"]. " - Name: " . $row["FirstName"]. " " . $row["LastName"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
